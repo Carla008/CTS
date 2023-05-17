d5366957646363a2b089bc90a8cb5afed3fdb87a
Carla008 <116124565+Carla008@users.noreply.github.com>
2023-05-17 17:12:54 +0300

    Add files via upload

---
+++ seminar8/Decorator/src/Main.java
@@ -0,0 +1,81 @@
+//Interfata Component
+interface Subscription{
+    String getDescription();
+    double getPrice();
+}
+//Implementare de baza a unui abonament
+class BasicSubscription implements Subscription{
+    @Override
+    public String getDescription(){
+        return "Abonament de baza";
+    }
+    @Override
+    public double getPrice(){
+        return 10.0;
+    }
+}
+
+//Decorator abstract
+abstract class SubscriptionDecorator implements Subscription{
+    protected Subscription subscription;
+    public SubscriptionDecorator(Subscription subscription){
+        this.subscription=subscription;
+    }
+    @Override
+    public String getDescription(){
+        return subscription.getDescription();
+    }
+    @Override
+    public double getPrice(){
+        return subscription.getPrice();
+    }
+}
+
+// Decorator pentru adaugarea de optiune de vizionare offline
+class OfflineViewingDecorator extends SubscriptionDecorator{
+    public OfflineViewingDecorator(Subscription subscription){
+        super(subscription);
+    }
+    @Override
+    public String getDescription(){
+        return subscription.getDescription()+ " , vizionare offline";
+    }
+    @Override
+    public double getPrice(){
+        return subscription.getPrice() + 5.0;
+    }
+}
+
+// Decorator pentru adaugarea de optiune de vizionare in calitate HD
+class HDStreamingDecorator extends SubscriptionDecorator{
+    public HDStreamingDecorator(Subscription subscription){
+        super(subscription);
+    }
+    @Override
+    public String getDescription(){
+        return subscription.getDescription() + ", vizionare in calitate HD";
+    }
+    @Override
+    public double getPrice(){
+        return subscription.getPrice() + 3.0;
+    }
+}
+
+// Codul client
+public class Main {
+    public static void main(String[] args) {
+
+        //Creeaza un abonament de baza
+        Subscription subscription=new BasicSubscription();
+
+        // Adauga optiune de vizionare offline
+        subscription=new OfflineViewingDecorator(subscription);
+
+        // Adauga optiune de vizionare in calitate HD
+        subscription = new HDStreamingDecorator(subscription);
+
+        // Afiseaza descrierea si pretul final al abonamentului
+        System.out.println("Descriere: " + subscription.getDescription());
+        System.out.println("Pret: "+subscription.getPrice());
+    }
+}
