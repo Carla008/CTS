33500b4a3dc9806a87386c372ebe1187ec3f2db1
Carla008 <116124565+Carla008@users.noreply.github.com>
2023-05-21 07:24:29 +0300

    Add files via upload

--- seminar7/Adapter/Example 1/src/Main.java
+++ seminar7/Adapter/Example 1/src/Main.java
@@ -1,22 +1,4 @@
-public interface Target {
-    void request();
-}
-public class Adapter implements Target{
-    private  Adaptee adaptee;
-    public  Adapter(Adaptee adaptee){
-        this.adaptee=adaptee;
-    }
-    @Override
-    public  void request(){
-        adaptee.specificRequest();
-    }
-}
-public class Adaptee{
-    public void specificRequest(){
-        System.out.println("Adaptee specific request");
-    }
-}
-public class Client{
+public class Main {
     public static void main(String[] args){
         Adaptee adaptee=new Adaptee();
         Target target=new Adapter(adaptee);
